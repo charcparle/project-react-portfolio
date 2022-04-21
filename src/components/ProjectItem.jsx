308023c195a143223e762d0575179a0318ffa598
Charles Cheng <charles.yhcheng@gmail.com>
2022-04-20 18:46:08 -0700

    Update CSS for animation of ProjectItems; Minor Update on link buttons for project items

--- src/components/ProjectItem.jsx
+++ src/components/ProjectItem.jsx
@@ -5,12 +5,14 @@ import { faArrowUpRightFromSquare } from '@fortawesome/free-solid-svg-icons'
 function ProjectItem({ projectName, stack, link }) {
   return (
     <div className="project-item">
-      <h4>
-        {projectName} &emsp;
-        <a href={link} target="_blank" rel="noreferrer">
-          <FontAwesomeIcon icon={faArrowUpRightFromSquare} />
-        </a>
-      </h4>
+      <div className="project-title">
+        <h4>{projectName} &emsp;</h4>
+        <button className="linkBtn">
+          <a href={link} target="_blank" rel="noreferrer">
+            <FontAwesomeIcon icon={faArrowUpRightFromSquare} />
+          </a>
+        </button>
+      </div>
       <h5>{stack}</h5>
     </div>
   )
